161b1ee32e300354d81d5a0df0e38839e1d14604
Eduardo <ejrb1234@gmail.com>
2024-08-16 20:29:39 -0400

    added profilePage

--- src/app/(pages)/dashboard/page.jsx
+++ src/app/(pages)/dashboard/page.jsx
@@ -1,13 +1,7 @@
-'use client'
-import { Button } from '@nextui-org/react'
-import { signOut } from 'next-auth/react'
-
-export default async function DashboardPage() {
+export default function DashboardPage() {
   return (
     <>
-      <Button color="primary" onClick={signOut}>
-        Cerrar sesión
-      </Button>
+      <h2>{`</DashboardPage>`}</h2>
     </>
   )
 }
