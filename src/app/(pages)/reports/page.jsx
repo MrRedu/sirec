49d6e1067caeeffb24882185c7f93a21172c590c
Eduardo <ejrb1234@gmail.com>
2024-08-17 21:02:45 -0400

    reportsPage

---
+++ src/app/(pages)/reports/page.jsx
@@ -0,0 +1,32 @@
+import { Section } from '@/components/atoms/ui/Section'
+import { Button, Card, CardFooter, Image } from '@nextui-org/react'
+
+export default function ReportsPage() {
+  return (
+    <Section>
+      <div className="flex items-center mb-4">
+        <h2 className="text-2xl font-bold leading-none tracking-tight text-gray-800  dark:text-white">{`Reportes`}</h2>
+      </div>
+
+      <Card
+        isFooterBlurred
+        radius="lg"
+        className="border-none max-w-[200px] max-h-[200px] bg-slate-500"
+      >
+        <Image width={200} height={200} src="/users.svg" alt="Users image" />
+        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <p className="text-tiny text-white/80">{`Usuarios`}</p>
+          <Button
+            className="text-tiny text-white bg-black/20"
+            variant="flat"
+            color="default"
+            radius="lg"
+            size="sm"
+          >
+            {`Generar reporte`}
+          </Button>
+        </CardFooter>
+      </Card>
+    </Section>
+  )
+}
