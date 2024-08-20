58e179b6ffb76d473281e84b9710e69e18414ffa
Eduardo <ejrb1234@gmail.com>
2024-08-19 20:08:40 -0400

    template literals

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -8,13 +8,13 @@ export default function NotFoundPage() {
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
           <h2 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
-            #404
+            {`#404`}
           </h2>
           <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">{`Esta página no existe`}</p>
           <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">{`No pudimos encontrar la página solicitada.`}</p>
           <Button color="primary" className="relative">
             <Link href="/" className="absolute inset-0" />
-            Volver a la página principal
+            {`Volver a la página principal`}
           </Button>
         </div>
       </div>
