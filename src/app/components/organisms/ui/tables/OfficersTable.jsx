5837a07c027ad5c6bf703297715dbd45f77c0dc1
Eduardo <ejrb1234@gmail.com>
2024-08-25 10:51:40 -0400

    working on customHook and Form to Officers

--- src/app/components/organisms/ui/tables/OfficersTable.jsx
+++ src/app/components/organisms/ui/tables/OfficersTable.jsx
@@ -2,16 +2,24 @@
 import propTypes from 'prop-types'
 import {
   Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
   Table,
   TableBody,
   TableCell,
   TableColumn,
   TableHeader,
   TableRow,
+  useDisclosure,
 } from '@nextui-org/react'
 import { useState } from 'react'
 import { Title } from '@/components/atoms/ui/Title'
 import { Pencil, Plus, Trash2 } from 'lucide-react'
+import { FormOfficer } from '../forms/FormOfficer'
+import { useOfficer } from '@/hooks/useOfficer'
 
 export const OfficersTable = ({
   ariaLabel = 'Example static collection table',
@@ -24,6 +32,19 @@ export const OfficersTable = ({
     setSelectedRow(row)
     console.log(selectedRow)
   }
+
+  const modalToAddOfficer = useDisclosure()
+  const modalToConfirmDelete = useDisclosure()
+
+  const {
+    officerData,
+    isLoading,
+    handleChange,
+    handleReset,
+    handleSubmit,
+    errors,
+  } = useOfficer()
+
   return (
     <>
       <div className="flex items-center justify-between mb-4">
@@ -32,7 +53,7 @@ export const OfficersTable = ({
           <Button
             color={'primary'}
             endContent={<Plus />}
-            // onPress={modalToAddRadio.onOpen}
+            onPress={modalToAddOfficer.onOpen}
           >{`Agregar`}</Button>
           <Button
             isIconOnly
@@ -49,7 +70,7 @@ export const OfficersTable = ({
             aria-label="Borrar"
             variant={selectedRow ? 'flat' : 'light'}
             isDisabled={!selectedRow}
-            // onPress={modalToConfirmDelete.onOpen}
+            onPress={modalToConfirmDelete.onOpen}
           >
             <Trash2 />
           </Button>
@@ -86,6 +107,96 @@ export const OfficersTable = ({
           ))}
         </TableBody>
       </Table>
+      {/* Modal to add officer */}
+      <Modal
+        isOpen={modalToAddOfficer.isOpen}
+        onOpenChange={modalToAddOfficer.onOpenChange}
+        isDismissable={false}
+        size="xl"
+        isKeyboardDismissDisabled={true}
+        scrollBehavior={'inside'}
+        backdrop={'blur'}
+      >
+        <ModalContent>
+          {onClose => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                {`Registrar funcionario`}
+              </ModalHeader>
+              <ModalBody>
+                <FormOfficer
+                  officerData={officerData}
+                  handleChange={handleChange}
+                  errors={errors}
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  color="danger"
+                  variant="light"
+                  onPress={() => {
+                    handleReset()
+                    onClose()
+                  }}
+                >
+                  {`Cerrar`}
+                </Button>
+                <Button
+                  color="primary"
+                  onClick={handleSubmit}
+                  isLoading={isLoading}
+                >
+                  {isLoading ? 'Creando...' : `Crear funcionario`}
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      {/* Modal to confirm delete */}
+      <Modal
+        isOpen={modalToConfirmDelete.isOpen}
+        onOpenChange={modalToConfirmDelete.onOpenChange}
+        isDismissable={false}
+        isKeyboardDismissDisabled={true}
+        scrollBehavior={'inside'}
+        backdrop={'blur'}
+      >
+        <ModalContent>
+          {onClose => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                {`Eliminar radio`}
+                EPA AQUI PENDIENTE CAMBIAR
+              </ModalHeader>
+              <ModalBody>
+                <p>{`¿Estás seguro de borrar el radio ${selectedRow.serial}?`}</p>
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  color="danger"
+                  variant="light"
+                  onPress={() => {
+                    onClose()
+                  }}
+                >
+                  {`Cerrar`}
+                </Button>
+                <Button
+                  color="primary"
+                  onClick={() => {
+                    // TODO: Hacer esta funcion
+                    // deleteOfficer(selectedRow.cedula)
+                    onClose()
+                  }}
+                >
+                  {`Eliminar radio`}
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   )
 }
