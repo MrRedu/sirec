c770c70b70daa5eed3873d296a6f1c28317eb190
Eduardo <ejrb1234@gmail.com>
2024-08-26 16:07:45 -0400

    assignments modules

--- src/app/components/organisms/ui/forms/FormAssignment.jsx
+++ src/app/components/organisms/ui/forms/FormAssignment.jsx
@@ -1,28 +1,30 @@
 'use client'
-import { useAssignments } from '@/hooks/useAssignments'
+import propTypes from 'prop-types'
 import { Button, Input, Spinner } from '@nextui-org/react'
 import { Search } from 'lucide-react'
 
-export const FormAssignment = () => {
-  const {
-    serial,
-    cedula,
-    errors,
-    handleChange,
-    isLoadingSerial,
-    isLoadingCedula,
-    handleSubmitSerial,
-    handleSubmitCedula,
-    handleSubmit,
-    isLoading,
-  } = useAssignments()
-
+export const FormAssignment = ({
+  serial,
+  cedula,
+  errors,
+  handleChange,
+  isLoadingSerial,
+  isLoadingCedula,
+  handleSubmitSerial,
+  handleSubmitCedula,
+  handleSubmit,
+}) => {
   return (
-    <form className="form flex flex-col gap-4" autoComplete="off">
+    <form
+      onSubmit={handleSubmit}
+      className="form flex flex-col gap-4"
+      autoComplete="off"
+    >
       <Input
         type="text"
         name="serial"
         label="Serial del radio"
+        isRequired
         value={serial}
         isReadOnly={errors.serial.color === 'success'}
         color={errors.serial.color}
@@ -45,6 +47,7 @@ export const FormAssignment = () => {
           type="text"
           name="cedula"
           label="Cédula del funcionario"
+          isRequired
           value={cedula}
           isReadOnly={errors.cedula.color === 'success'}
           color={errors.cedula.color}
@@ -62,17 +65,18 @@ export const FormAssignment = () => {
           }
         />
       )}
-
-      {/* {errors.cedula.color === 'success' && ( */}
-      <Button
-        type="submit"
-        color="primary"
-        disabled={isLoading}
-        onClick={handleSubmit}
-      >
-        {isLoading ? <Spinner size="xs" /> : 'Asignar'}
-      </Button>
-      {/* )} */}
     </form>
   )
+}
+
+FormAssignment.propTypes = {
+  serial: propTypes.string,
+  cedula: propTypes.string,
+  errors: propTypes.object,
+  handleChange: propTypes.func,
+  isLoadingSerial: propTypes.bool,
+  isLoadingCedula: propTypes.bool,
+  handleSubmitSerial: propTypes.func,
+  handleSubmitCedula: propTypes.func,
+  handleSubmit: propTypes.func,
 }
